2ebad21bedab36868e2cd17a84e7b6e15b1ef97d
GiorgioRussi <shonetod7775@gmail.com>
2024-04-21 01:17:57 +0300

    Delete FireFoxDriver import.

--- src/test/java/ru/yandex/praktikum/FAQTest.java
+++ src/test/java/ru/yandex/praktikum/FAQTest.java
@@ -8,7 +8,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static org.junit.Assert.assertTrue;
 
